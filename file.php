92e460644e92a98228d90a1b2ea7650132a78967
Mixailhr <mixailhr@gmail.com>
2016-05-14 23:58:16 +0300

    второй коммит

--- file.php
+++ file.php
@@ -1,1 +1,11 @@
-<?php echo "Hello Мир!)))"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<div><p>1232123123tyvhjjkhb</p></div>
+<?php echo "Hello Мир!)))"?>	
+</body>
+</html>
