f3619ae79cd5df28ac70463093f3a85879d61a04
Mixailhr <mixailhr@gmail.com>
2016-05-15 17:46:24 +0300

    Создан файл send.php в ветке мастер

---
+++ send.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">первая колонка</div>
+				<div class="col-sm-6">вторая колонка</div>
+			</div>
+		</div>
+	</section>
+</head>
+<body>
+	
+</body>
+</html>
